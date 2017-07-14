80f08622c1c93507e906cecf4435962597d36e06
Riki <rick.libby.91@gmail.com>
2017-07-14 19:50:33 +0000

    add procfile and .env database info

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 "use strict";
-
+require('dotenv').config();
 // Basic express setup:
 
 const PORT          = 8080
@@ -9,7 +9,7 @@ const {MongoClient} = require('mongodb')
 const app           = express()
 const ObjectId      = require('mongodb').ObjectID
 
-const MONGODB_URI = 'mongodb://supermarche-madame-47422.herokuapp.com:27017/tweeter'
+const MONGODB_URI = process.env.MONGODB_URI
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
@@ -23,20 +23,18 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // This simple interface layer has a big benefit: we could switch out the
   // actual database it uses and see little to no changes elsewhere in the code
   // (hint hint).
-
   // Because it exports a function that expects the `db` as a parameter, we can
   // require it and pass the `db` parameter immediately:
-  const DataHelpers = require("./lib/data-helpers.js")(db, ObjectId);
+  const DataHelpers = require("./lib/data-helpers.js")(db, ObjectId)
   
   // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
   // so it can define routes that use it to interact with the data layer.
-  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers)
   
   // Mount the tweets routes at the "/tweets" path prefix:
-  app.use("/tweets", tweetsRoutes);
-
-  app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
-  });
-
+  app.use("/tweets", tweetsRoutes)
 })
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT)
+});
